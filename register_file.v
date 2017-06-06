f1344de7201b4e9fdbc8a85da2a5853f7049ab4a
skystar-p <wogus150@naver.com>
2017-06-06 21:50:42 +0900

    register file refactored

--- register_file.v
+++ register_file.v
@@ -12,43 +12,24 @@ module register_file(
     );
 
     // total four registers
-    reg [7:0] reg0;
-    reg [7:0] reg1;
-    reg [7:0] reg2;
-    reg [7:0] reg3;
+    reg [7:0] register [0:3];
 
     // initalize to 0
     initial begin
-        reg0 <= 8'b0;
-        reg1 <= 8'b0;
-        reg2 <= 8'b0;
-        reg3 <= 8'b0;
+        register[0] <= 8'b0;
+        register[1] <= 8'b0;
+        register[2] <= 8'b0;
+        register[3] <= 8'b0;
     end
 
     // main loop
     always @(posedge clock) begin
         if (signal_regwrite) begin
-            case (write_reg)
-                2'b00: reg0 <= write_data;
-                2'b01: reg1 <= write_data;
-                2'b10: reg2 <= write_data;
-                2'b11: reg3 <= write_data;
-            endcase
+            register[write_reg] <= write_data;
         end
         else begin
-            case (read_reg1)
-                2'b00: output_reg1 <= reg0;
-                2'b01: output_reg1 <= reg1;
-                2'b10: output_reg1 <= reg2;
-                2'b11: output_reg1 <= reg3;
-            endcase
-            
-            case (read_reg2)
-                2'b00: output_reg2 <= reg0;
-                2'b01: output_reg2 <= reg1;
-                2'b10: output_reg2 <= reg2;
-                2'b11: output_reg2 <= reg3;
-            endcase
+            output_reg1 <= register[read_reg1];
+            output_reg2 <= register[read_reg2];
         end
     end
 
