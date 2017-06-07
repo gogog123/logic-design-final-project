cbaa843f75166c84182465a0278d00cd652480a6
skystar-p <wogus150@naver.com>
2017-06-07 21:08:55 +0900

    control unit refactored into combinational logic

--- control_unit.v
+++ control_unit.v
@@ -3,62 +3,30 @@
 module control_unit(
     input clock,
     input [1:0] opcode,
-    output reg signal_memtoreg,
-    output reg signal_regwrite,
-    output reg signal_alusrc,
-    output reg signal_branch,
-    output reg signal_memread,
-    output reg signal_memwrite,
-    output reg signal_regdst,
-    output reg signal_aluop
+    output signal_memtoreg,
+    output signal_regwrite,
+    output signal_alusrc,
+    output signal_branch,
+    output signal_memread,
+    output signal_memwrite,
+    output signal_regdst,
+    output signal_aluop
     );
 
-    always @(posedge clock) begin
-        case (opcode)
-            2'b00: begin
-                signal_regdst <= 1'b1;
-                signal_regwrite <= 1'b1;
-                signal_alusrc <= 1'b0;
-                signal_branch <= 1'b0;
-                signal_memread <= 1'b0;
-                signal_memwrite <= 1'b0;
-                signal_memtoreg <= 1'b0;
-                signal_aluop <= 1'b1;
-            end
+    wire A;
+    wire B;
 
-            2'b01: begin
-                signal_regdst <= 1'b0;
-                signal_regwrite <= 1'b1;
-                signal_alusrc <= 1'b1;
-                signal_branch <= 1'b0;
-                signal_memread <= 1'b1;
-                signal_memwrite <= 1'b0;
-                signal_memtoreg <= 1'b1;
-                signal_aluop <= 1'b0;
-            end
+    assign A = opcode[1];
+    assign B = opcode[0];
 
-            2'b10: begin
-                signal_regdst <= 1'b0; // don't care
-                signal_regwrite <= 1'b0;
-                signal_alusrc <= 1'b1;
-                signal_branch <= 1'b0;
-                signal_memread <= 1'b0;
-                signal_memwrite <= 1'b1;
-                signal_memtoreg <= 1'b0; // don't care
-                signal_aluop <= 1'b0;
-            end
-
-            2'b11: begin
-                signal_regdst <= 1'b0; // don't care
-                signal_regwrite <= 1'b0;
-                signal_alusrc <= 1'b0;
-                signal_branch <= 1'b1;
-                signal_memread <= 1'b0;
-                signal_memwrite <= 1'b0;
-                signal_memtoreg <= 1'b0; // don't care
-                signal_aluop <= 1'b0;
-            end
-        endcase
-    end
+    // K-map minimization (combinational logic)
+    assign signal_regdst = ~A && ~B;
+    assign signal_regwrite = ~A;
+    assign signal_alusrc = (~A && B) || (A && ~B);
+    assign signal_branch = A && B;
+    assign signal_memread = ~A && B;
+    assign signal_memwrite = A && ~B;
+    assign signal_memtoreg = B;
+    assign signal_aluop = ~A && ~B;
 
 endmodule
