b29783cb6676de4a1b1aa361e2760bef8d197744
skystar-p <wogus150@naver.com>
2017-06-09 15:48:01 +0900

    instruction tester code added

---
+++ instruction/tester.v
@@ -0,0 +1,24 @@
+module tester(
+    input address,
+    input clear,
+    output instruction
+);
+
+// memory for instruction
+wire [7:0] memory [0:31];
+
+
+// Instruction start
+
+assign memory[0] = 8'b01000100;
+assign memory[1] = 8'b01001001;
+assign memory[2] = 8'b00011000;
+assign memory[3] = 8'b10001001;
+assign memory[4] = 8'b11000011;
+
+// Instruction end
+
+// output instruction
+assign instruction = memory[address];
+
+endmodule
